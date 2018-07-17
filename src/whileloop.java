4ec27bbb9a2724016542fae61b066afdecaec296
GBTC441004UR <GBTC441004UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-17 13:35:39 -0400

    initial commit

---
+++ src/whileloop.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class whileloop {
+
+        public static void main(String[] args) {
+
+            Scanner keyboard = new Scanner(System.in);
+            int secretNumber, guess = 0;//guess is initialized to 0
+
+            secretNumber = 123;
+
+            System.out.println("I'm thinking of a number between 1 and 1000");
+            System.out.print("Enter the number:");
+            guess = keyboard.nextInt();
+
+
+            while ( (guess != secretNumber) && (guess != 5)  )
+            {
+                System.out.println("\nYou are wrong. Try again.");
+                System.out.println("Enter the number: ");
+                guess = keyboard.nextInt();
+
+            }
+
+            if (guess == 5){
+                System.out.println("you lost");}
+            else {
+                System.out.println("You are correct. You win a prize!");}
+            keyboard.close();
+        }
+}
